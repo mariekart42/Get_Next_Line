726ecf6873afd9fceb405f79c2ed5e6de0c245b4
thedatab0y <114252614+thedatab0y@users.noreply.github.com>
2022-10-09 17:36:41 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -11,9 +11,9 @@ char	*get_next_line(int fd);
 size_t	ft_strlen(char *str);
 char	*get_line(char *buffer, int fd);
 char	*get_read_line(char *buffer);
-char	ft_get_rest(char *buffer);
-char	strchr(char *str, char c);
-char	ft_strjoin(char *str1, char *str2);
+char	*ft_get_rest(char *buffer);
+char	*strchr(char *str, char c);
+char	*ft_strjoin(char *str1, char *str2);
 
 
 #endif
